41c2c04e29b819b19bde968776c69e81cef22275
Norbert Herbert <norbert.herbert@actility.com>
2022-01-21 09:02:05 +0100

    2 new test scripts for debugging

---
+++ sensative/strips/decode_working_example.js
@@ -0,0 +1,15 @@
+let driver = require('./index.js');
+
+const example = {
+    bytes: "FFFF13355E1201",
+    fPort: 1,
+    time: "2020-08-02T20:00:00.000+05:00",
+}
+
+const decoded = driver.decodeUplink({ bytes: Buffer.from(example.bytes, 'hex'), fPort: example.fPort});
+console.log('decodeUplink output:');
+console.log(JSON.stringify(decoded, null, 4));
+
+console.log('extractPoints output:');
+const extracted = driver.extractPoints({ message: decoded, time: example.time });
+console.log(JSON.stringify(extracted, null, 4));
