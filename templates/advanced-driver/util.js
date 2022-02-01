79bd448d1c53caeb8d4a7259e82e62d302406df5
Norbert Herbert <norbert.herbert@actility.com>
2022-02-01 11:51:18 +0100

    Driver Development Guide

--- templates/advanced-driver/util.js
+++
@@ -1,9 +0,0 @@
-function readShort(bin) {
-    var result = bin & 0xffff;
-    if (0x8000 & result) {
-        result = -(0x010000 - result);
-    }
-    return result;
-}
-
-exports.readShort = readShort;
