261f1d9913b6e3900b9d224c3e5be4ce77e2e427
Yordan Zaychev <yordanzaychev@gmail.com>
2022-03-23 14:41:07 +0200

    Initial MClimate Devices

---
+++ MClimate/flood-sensor/index.js
@@ -0,0 +1,71 @@
+
+function decodeUplink(input) {
+	try{
+		var bytes = input.bytes;
+		var decbin = function(number) {
+			return parseInt(number, 10).toString(2);
+		};
+		var byteArray = bytes.map(function(byte) {
+			var number = decbin(byte);
+			return Array(9 - number.length).join('0') + number;
+		});
+		var messageTypes = [ 'keepalive', 'testButtonPressed', 'floodDetected', 'fraudDetected', 'fraudDetected' ];
+		toBool = function(value) {
+			return value == '1';
+		};
+		shortPackage = function(byteArray) {
+			return {
+					reason: messageTypes[parseInt(byteArray[0].slice(0, 3),2)],
+					boxTamper: toBool(byteArray[0][4]),
+					flood: toBool(byteArray[0][6]),
+					battery: (parseInt(byteArray[1], 2) * 16)/1000,
+			};
+		};
+		longPackage = function(byteArray) {
+			return {
+					reason: messageTypes[parseInt(byteArray[0].slice(0, 3),2)],
+					boxTamper: toBool(byteArray[0][4]),
+					flood: toBool(byteArray[0][6]),
+					battery: (parseInt(byteArray[1], 2) * 16)/1000,
+					temperature: parseInt(byteArray[2], 2),
+			};
+		};
+		if(byteArray.length < 2){
+			throw new Error('Unhandled data');
+		}
+		if (byteArray.length > 2) {
+			return longPackage(byteArray);
+		} else {
+			return shortPackage(byteArray);
+		}
+	}catch (e) {
+        throw new Error('Unhandled data');
+    }
+}
+
+
+function decodeDownlink(input) {
+}
+
+function encodeDownlink(input) {
+}
+
+function extractPoints(input) {
+    let result = {};
+    let elements = input.message.volumes;
+    if (typeof elements !== "undefined") {
+        result.volume = [];
+        elements.forEach(element => {
+            result.volume.push({
+                eventTime: element.time,
+                value: element.volume
+            })
+        });
+    }
+    return result;
+}
+
+exports.decodeUplink = decodeUplink;
+exports.decodeDownlink = decodeDownlink;
+exports.encodeDownlink = encodeDownlink;
+exports.extractPoints = extractPoints;
